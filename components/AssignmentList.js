76538999609954ad4aaa611d5a44f18805a25b54
Haditya Fajri <haditya.fajri@investree.id>
2022-10-28 14:05:11 +0700

    Create feature tag

--- components/AssignmentList.js
+++ components/AssignmentList.js
@@ -7,11 +7,17 @@ export default {
         <h2 class="font-bold mb-2">{{title}} <span>({{assignments.length}})</span></h2>
 
         <div class="flex gap-2">
-          <button v-for="tag in tags" class="border rounded px-1 py-px text-xs">{{tag}}</button>
+          <button 
+            @click="currentTag = tag"
+            v-for="tag in tags" 
+            class="border rounded px-1 py-px text-xs"
+            :class="{
+              'border-blue-500 text-blue-500': tag === currentTag
+            }">{{tag}}</button>
         </div>
         
         <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-            <assignment v-for="assignment in assignments" :key="assignment.id" :assignment="assignment" ></assignment>
+            <assignment v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment" ></assignment>
         </ul>
         </section>
     `,
@@ -19,9 +25,26 @@ export default {
     assignments: Array,
     title: String,
   },
+  data() {
+    return {
+      currentTag: "all",
+    };
+  },
   computed: {
+    filteredAssignments() {
+      if (this.currentTag === "all") {
+        return this.assignments;
+      }
+
+      return this.assignments.filter(
+        (assignment) => assignment.tag === this.currentTag
+      );
+    },
     tags() {
-      return new Set(this.assignments.map((assignment) => assignment.tag));
+      return [
+        "all",
+        ...new Set(this.assignments.map((assignment) => assignment.tag)),
+      ];
     },
   },
 };
